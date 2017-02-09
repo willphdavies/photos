ffa72e7e0984353acbd93e2bf988c8e21e2446b1
William Davies <william.davies@soliddigital.com>
2017-02-08 22:59:54 -0800

    adding basic search

--- src/app/album/filter.pipe.ts
+++ src/app/album/filter.pipe.ts
@@ -8,7 +8,6 @@ import {Album} from './album.model';
 })
 export class AlbumFilterPipe implements PipeTransform {
     transform(collection: Album[], term: string): Album[] {
-        console.log(term);
         if (term && term.length>0) {
             return _.filter(collection, (item) => item.title.search(term) >= 0);
         } else {
